5a63b7c8437552134ab4ab98c8e0e9b711815e1d
PeskyPotato <peskypotato@protonmail.com>
2024-11-24 13:06:53 +0100

    Read secret key from env variable

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, render_template, flash
+import os
 import yt_dlp
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
